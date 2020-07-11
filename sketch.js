87ee45d7625c15b8502d16c50010ef730f49b224
zoya-azmi <64300845+zoya-azmi@users.noreply.github.com>
2020-07-11 11:34:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var trackImg, groundImg, car1Img, car2Img, car3Img, car4Img;
+
+
+function preload(){
+  car1Img = loadImage("images/car1.png");
+  car2Img = loadImage("images/car2.png");
+  car3Img = loadImage("images/car3.png");
+  car4Img = loadImage("images/car4.png");
+  trackImg = loadImage("images/track.jpg");
+  groundImge = loadImage("images/ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
